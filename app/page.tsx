41744a435254e8ae492d2b8158e02dba6d14d01d
kdjadeja21 <krushnasinh.jadeja14545@marwadieducation.edu.in>
2024-04-17 16:24:50 +0530

    Added Loading Component

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import store from "./store";
+import Loading from "./Loading/Loading";
 
 export default function Home({
   children,
@@ -15,18 +16,22 @@ export default function Home({
   const session = useSession();
   return (
     <>
-      <Provider store={store}>
-        <Toaster />
-        {session.status === "authenticated" && (
-          <Suspense>
-            <Nav />
-          </Suspense>
-        )}
-        <main className="p-4 md:p-10 mx-auto max-w-7xl">
-          <WeightChart />
-        </main>
-        {children}
-      </Provider>
+      <Suspense fallback={<Loading />}>
+        <Provider store={store}>
+          <Toaster />
+          {session.status === "authenticated" && (
+            <Suspense fallback={<Loading />}>
+              <Nav />
+            </Suspense>
+          )}
+          <main className="p-4 md:p-10 mx-auto max-w-7xl">
+            <Suspense fallback={<Loading />}>
+              <WeightChart />
+            </Suspense>
+          </main>
+          {children}
+        </Provider>
+      </Suspense>
     </>
   );
 }
